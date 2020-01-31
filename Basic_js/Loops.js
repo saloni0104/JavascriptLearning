4b109614f5dad9c952406cba5df4652b8033f352
saloni <suparekh01@gmail.com>
2020-01-31 20:03:47 +0530

    Loops

---
+++ Basic_js/Loops.js
@@ -0,0 +1,43 @@
+
+/*
+Loops and iteration
+*/
+
+// for loop
+for (var i = 1; i <= 20; i += 2) {
+    console.log(i);
+}
+
+
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+for (var i = 0; i < john.length; i++) {
+    console.log(john[i]);
+}
+
+// While loop
+var i = 0;
+while(i < john.length) {
+    console.log(john[i]);
+    i++;
+}
+
+
+// continue and break statements
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+// Looping backwards
+for (var i = john.length - 1; i >= 0; i--) {
+    console.log(john[i]);
+}
+
+
